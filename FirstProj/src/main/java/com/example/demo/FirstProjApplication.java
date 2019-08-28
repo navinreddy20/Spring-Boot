65648420b1a1f90c5985d99b7e747618825fee97
Telusko <Telusko@SuperComputer>
2019-08-28 14:00:37 +0530

    Spring Tool Suite | Spring Boot IDE

---
+++ FirstProj/src/main/java/com/example/demo/FirstProjApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjApplication.class, args);
+		System.out.println("Welcome to boot...");
+	}
+
+}
