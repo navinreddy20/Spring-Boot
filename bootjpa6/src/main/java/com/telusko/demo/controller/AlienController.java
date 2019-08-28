3e14d4b93e0cc3cd96368541ef022c88530595b9
Telusko <Telusko@SuperComputer>
2019-08-28 15:21:00 +0530

    Spring Boot | MVC | REST Post Example Part 8

---
+++ bootjpa6/src/main/java/com/telusko/demo/controller/AlienController.java
@@ -0,0 +1,53 @@
+package com.telusko.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.telusko.demo.dao.AlienRepo;
+import com.telusko.demo.model.Alien;
+
+@RestController
+public class AlienController
+{
+	@Autowired
+	AlienRepo repo;
+
+	@RequestMapping("/")
+	public String home()
+	{
+		return "home.jsp";
+	}
+	@PostMapping(path="/alien",consumes= {"application/json"})
+	public Alien addAlien(@RequestBody Alien alien)
+	{
+		repo.save(alien);
+		return alien;
+	}
+	@GetMapping(path="/aliens")
+	public List<Alien> getAliens()
+	{
+		return repo.findAll();
+		
+		
+	}
+	@RequestMapping("/alien/{aid}")
+	public Optional<Alien> getAlien(@PathVariable("aid")int aid)
+	{
+		return repo.findById(aid);
+		
+		
+	}
+}
---
+++ bootjpa6/src/main/java/com/telusko/demo/dao/AlienRepo.java
@@ -0,0 +1,14 @@
+package com.telusko.demo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import com.telusko.demo.model.Alien;
+
+import java.util.List;
+
+public interface AlienRepo extends JpaRepository<Alien,Integer>
+{
+
+}
