a7fc949f2b08798383251aaa8759c293bc28f350
Telusko <Telusko@SuperComputer>
2019-08-28 14:15:37 +0530

    Web App using Spring Boot

---
+++ MyWebApp/src/main/java/com/telusko/demo/HomeController.java
@@ -0,0 +1,16 @@
+package com.telusko.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController
+{
+	@RequestMapping("home")
+	public String home()
+	{
+		System.out.println("hi");
+		return "home.jsp";
+	}
+}
